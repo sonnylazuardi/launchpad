0f9ca5d56b9b56bfd424d1ed67d074d0a616d40c
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-03 18:05:30 -0700

    Install prettier + bundle in one script

--- installPrettier.js
+++ installPrettier.js
@@ -1,11 +1,62 @@
 var npm = require('npm-programmatic');
-npm.install(['CompuIves/custom-prettier-codesandbox'], {
-  //npm.install(['prettier@compuives/prettier'], {
-  // cwd to seperate directory should be working but it isn't???
-  cwd: './node_modules',
-  save: true
-}).then(function() {
-  console.log('Success!!');
-}).catch(function() {
-  console.log('unable to install package');
-});
+var rollup = require('rollup');
+var fs = require('fs');
+
+npm
+  .install(['CompuIves/custom-prettier-codesandbox'], {
+    // cwd to seperate directory should be working but it isn't???
+    cwd: './node_modules',
+    save: true,
+  })
+  .then(function() {
+    console.log('Success!!');
+    rollup
+      .rollup({
+        entry: 'node_modules/prettier/index.js',
+      })
+      .then(function(bundle) {
+        bundle.write({
+          format: 'iife',
+          dest: 'prettierBundle/prettier.js',
+        });
+      })
+      .then(function() {
+        rollup
+          .rollup({
+            entry: 'node_modules/prettier/parser-babylon.js',
+          })
+          .then(function(bundle) {
+            bundle.write({
+              format: 'iife',
+              dest: 'prettierBundle/prettier.js',
+            });
+          });
+      })
+      .then(function() {
+        rollup
+          .rollup({
+            entry: 'node_modules/prettier/parser-json.js',
+          })
+          .then(function(bundle) {
+            bundle.write({
+              format: 'iife',
+              dest: 'prettierBundle/prettier.js',
+            });
+          });
+      })
+      .then(function() {
+        rollup
+          .rollup({
+            entry: 'node_modules/prettier/parser-postcss.js',
+          })
+          .then(function(bundle) {
+            bundle.write({
+              format: 'iife',
+              dest: 'prettierBundle/prettier.js',
+            });
+          });
+      });
+  })
+  .catch(function() {
+    console.log('unable to install package');
+  });
