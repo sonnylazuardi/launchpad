8abe095400bca0c055b6f2d9d212b898dffdd658
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-03 17:02:50 -0700

    Change rollup file to include entire node_module

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,27 @@
 import babel from 'rollup-plugin-babel';
 
-export default {
-  entry: 'node_modules/prettier/index.js',
-  dest: 'prettier_build/prettier_build.js',
-  format: 'iife',
-  sourceMap: 'inline',
-}
+export default [ 
+  { entry: 'node_modules/prettier/index.js',
+    dest: 'prettierBundle/prettier.js',
+    format: 'iife',
+    sourceMap: 'inline',
+  }, {
+    entry: 'node_modules/prettier/parser-babylon.js',
+    dest: 'prettierBundle/prettier.js',
+    format: 'iife',
+    sourceMap: 'inline'
+  }, {
+    entry: 'node_modules/prettier/parser-json.js',
+    dest: 'prettierBundle/prettier.js',
+    format: 'iife',
+    sourceMap: 'inline'
+  }, {
+    entry: 'node_modules/prettier/parser-postcss.js',
+    dest: 'prettierBundle/prettier.js',
+    format: 'iife',
+    sourceMap: 'inline'
+  }
+]
 
 // might need to install babel dependencies
 // run with ./node_modules/.bin/rollup -c
