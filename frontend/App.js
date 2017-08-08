e2ddd6f81ffa7b1f46750e856081bde9bd4c9447
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-07 17:34:26 -0700

    Fix flow errors

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import {
   ApolloProvider,
   createNetworkInterface,
 } from 'react-apollo';
+import type { MiddlewareInterface } from 'apollo-client';
 import PadContainer from './PadContainer';
 import ListContainer from './ListContainer';
 
@@ -13,20 +14,26 @@ const networkInterface = createNetworkInterface({
   uri: (process.env.REACT_APP_LAUNCHPAD_API_URL: any),
 });
 
-networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      if (!req.options.headers) {
-        req.options.headers = {};
-      }
-      const token = localStorage.getItem('LAUNCHPAD_TOKEN');
-      if (token) {
-        req.options.headers['authorization'] = `Bearer: ${token}`;
-      }
-      next();
+networkInterface.use(
+  ([
+    {
+      applyMiddleware(req, next) {
+        if (!req.options.headers) {
+          req.options.headers = {};
+        }
+        const token = localStorage.getItem('LAUNCHPAD_TOKEN');
+        if (token) {
+          if (req.options.headers) {
+            (req.options.headers: { [key: string]: string })[
+              'authorization'
+            ] = `Bearer: ${token}`;
+          }
+        }
+        next();
+      },
     },
-  },
-]);
+  ]: Array<MiddlewareInterface>),
+);
 
 const apolloClient = new ApolloClient({
   networkInterface,
