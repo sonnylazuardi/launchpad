b178708fec711bbcac12f3a36b6cd72c99b9dcea
Ramya Nagarajan <ramyanaga.99@gmail.com>
2017-08-07 20:27:55 -0700

    Update travis + tests

--- frontend/EditorLinter.js
+++ frontend/EditorLinter.js
@@ -2,24 +2,25 @@
 
 import { getLinter } from './services/CodeService';
 
-export default async function register(CodeMirror: any) {
-  const lint = await getLinter();
-
-  CodeMirror.registerGlobalHelper(
-    'lint',
-    'babel',
-    () => true,
-    (text, options) => {
-      const errors = lint(text);
-      return errors.map(error => ({
-        message: error.message,
-        severity: error.severity === 2 ? 'error' : 'warning',
-        from: CodeMirror.Pos(error.line - 1, error.column),
-        to: CodeMirror.Pos(
-          (error.endLine || error.line) - 1,
-          error.endColumn || error.column + 1,
-        ),
-      }));
-    },
-  );
+// XXX for some reason the compilation breaks if you switch to async/await here
+export default function register(CodeMirror: any) {
+  getLinter().then(lint => {
+    CodeMirror.registerGlobalHelper(
+      'lint',
+      'babel',
+      () => true,
+      (text, options) => {
+        const errors = lint(text);
+        return errors.map(error => ({
+          message: error.message,
+          severity: error.severity === 2 ? 'error' : 'warning',
+          from: CodeMirror.Pos(error.line - 1, error.column),
+          to: CodeMirror.Pos(
+            (error.endLine || error.line) - 1,
+            error.endColumn || error.column + 1,
+          ),
+        }));
+      },
+    );
+  });
 }
